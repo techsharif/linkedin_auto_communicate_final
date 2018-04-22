bfc31b3de0e53ec09ad663e597956c9cbcf0d534
vajin1125 <va.jin1125@hotmail.com>
2018-04-23 07:35:55 +1000

    04/23
    some files deleted

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^account/$', views.account, name='account'),
-    url(r'^signup/$', views.signup, name='signup'),
-]
