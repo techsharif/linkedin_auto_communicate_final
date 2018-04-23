253a4feffd3359cf69d21ee442a9c687fc25c236
vajin1125 <va.jin1125@hotmail.com>
2018-04-23 22:53:35 +1000

    4/23 modify some code
    existign user is done
    suspended user and limited user is comment code now yet.

--- checkuser/checkuser.py
+++ checkuser/checkuser.py
@@ -29,18 +29,20 @@ def exist_user():
 
 
 def suspended_user():
-    suspended_account = driver.findElement(By.xpath("//*[contains(.,'account suspended')]"))
-    if (suspended_account == null):
-        return False
-    else:
-        return True
+    pass
+    # suspended_account = driver.findElement(By.xpath("//*[contains(.,'account suspended')]"))
+    # if (suspended_account == null):
+    #     return False
+    # else:
+    #     return True
 
 def limited_user():
-    restricted_account = driver.findElement(By.xpath("//*[contains(.,'temporarily restricted')]"))
-    if (restricted_account == null):
-        return False
-    else:
-        return True
+    pass
+    # restricted_account = driver.findElement(By.xpath("//*[contains(.,'temporarily restricted')]"))
+    # if (restricted_account == null):
+    #     return False
+    # else:
+    #     return True
 
 
 if __name__ == '__main__':
@@ -78,14 +80,14 @@ if __name__ == '__main__':
     else:
         print("That user is not exist in Linkedin.")
 
-    # check if user is suspended
-    if suspended_user():
-        print("That user is suspended.")
-    else:
-        print("That user is not suspended.")
+    # # check if user is suspended
+    # if suspended_user():
+    #     print("That user is suspended.")
+    # else:
+    #     print("That user is not suspended.")
 
-    # check if user is has any limitation
-    if limited_user():
-        print("That user has been restricted.")
-    else:
-        print("Thank user is not restricted.")
+    # # check if user is has any limitation
+    # if limited_user():
+    #     print("That user has been restricted.")
+    # else:
+    #     print("Thank user is not restricted.")
