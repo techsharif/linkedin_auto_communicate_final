ad697e68adb1d1449b7bc72d1d4f5271df587be3
Chetna <evolver.chetna@gmail.com>
2018-04-26 11:40:15 +0530

    04/26- Working on Messenger Campaign home page

--- messenger/models.py
+++ messenger/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+try:
+    from django.utils.encoding import force_text
+except ImportError:
+    from django.utils.encoding import force_unicode as force_text
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Campaign(TimeStampedModel):
+    id = models.AutoField(primary_key=True)
+    account_id = models.IntegerField(db_index=True)
+    title = models.CharField(max_length=100, db_index=True)
+    status = models.CharField(max_length=100, db_index=True)
+    
+    def __str__(self):
+        return force_text(str(self.account_id) + "  " + self.title)
+
+    class Meta():
+        abstract = False
+        # db_table = 'campaigns'
+
+
+class Contact(TimeStampedModel):
+    id = models.AutoField(primary_key=True)
+    account_id = models.IntegerField(db_index=True)
+    campaign_id = models.IntegerField(db_index=True, blank=True, null=True)
+    status = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True)
+    company = models.CharField(max_length=100, db_index=True, blank=True, null=True)
+    industry = models.CharField(max_length=100, db_index=True, blank=True, null=True)
+    location = models.CharField(max_length=100, db_index=True, blank=True, null=True)
+    latest_acitvity = models.DateTimeField()
+    
+    def __str__(self):
+        return force_text(str(self.account_id) + "  " + self.name)
+
+    class Meta():
+        abstract = False
+        # db_table = 'contacts'
+
+
+
+class CampaignSetp(TimeStampedModel):
+    id = models.AutoField(primary_key=True)
+    campaign_id = models.IntegerField(db_index=True, blank=True, null=True)
+    setp_number = models.IntegerField(db_index=True, default=1)
+    message = models.TextField()
+    action = models.CharField(max_length=100, db_index=True)
+    
+
+    class Meta():
+        abstract = False
+        # db_table = 'campaign_setps'
--- messenger/views.py
+++ messenger/views.py
@@ -14,8 +14,11 @@ from django.contrib.auth.decorators import login_required
 
 
 # local imports
+from messenger.models import Campaign
 
-
-@login_required
-def messenger_home(request):
-    return render(request, 'messenger/messenger.html')
+# @login_required
+def messenger_home(request, account_id):
+    response = {}
+    campaigns = Campaign.objects.all()
+    response = {'campaigns': campaigns}
+    return render(request, 'messenger/messenger.html', response)
