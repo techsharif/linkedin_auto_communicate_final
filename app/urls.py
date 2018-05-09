12fe61e550885416f9e640701689fc053995137f
Sharif Chowdhury <sharif.cse.hstu@gmail.com>
2018-05-09 17:30:14 +0600

    Complete registration with sending activation link Activate account Update values
    :todo:
    need to design invalid_activation.html, register_done.html

--- app/urls.py
+++ app/urls.py
@@ -2,50 +2,50 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from . import views
 
-
 urlpatterns = [
     # These are for user/page on our own
     url(r'^$', views.HomeView.as_view(), name='index'),
-    #url(r'^login/$', views.login, name='login'),
-    #url(r'^logout/$', views.logout, name='logout'),    
+    # url(r'^login/$', views.login, name='login'),
+    # url(r'^logout/$', views.logout, name='logout'),
     # confirm email
     # change password
     # forgot passsword
     url(r'^login/$',
         auth_views.LoginView.as_view(template_name='registration/login.html'),
         name='login'),
-    url(r'^logout/$', 
+    url(r'^logout/$',
         auth_views.LogoutView.as_view(template_name='registration/logged_out.html'),
         name='logout'),
-    url(r'^forgotpw', 
+    url(r'^forgotpw',
         auth_views.PasswordResetView.as_view(
             template_name='registration/page-forgot-password.html'),
-        name='forgotpw'),                  
-    url(r'password_reset_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', 
+        name='forgotpw'),
+    url(r'password_reset_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
         auth_views.PasswordResetConfirmView.as_view(
             template_name='registration/page-password_reset_confirm.html'),
         name='password_reset_confirm'),
     url(r'password_reset_complete',
         auth_views.PasswordResetDoneView.as_view(
             template_name='registration/page-password_reset_complete.html'),
-        name='password_reset_complete'),        
-    url(r'password_reset_done', 
+        name='password_reset_complete'),
+    url(r'password_reset_done',
         auth_views.PasswordResetDoneView.as_view(
             template_name='registration/page-password_reset_done.html'),
         name='password_reset_done'),
-    
+
     url(r'^register/$', views.RegisterView.as_view(), name='register'),
     url(r'^registered/$', views.TemplateView.as_view(
-        template_name='registration/register_done.html'), 
+        template_name='registration/register_done.html'),
         name='register_done'),
     url(r'^subscription/$', views.SubsriptionView.as_view(), name='subscription'),
     url(r'^profile/$', views.ProfileView.as_view(), name='profile'),
-    
-    
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.ActivateAccount.as_view(), name='activate'),
+
     # these stuff will be for account on linkedIN
-    #url(r'^accounts/settings/(?P<pk>[\d+])$', views.account, name='account-settings'),
-    #url(r'^accounts/add/$', views.update_account, name='add-account'),
-    #url(r'^accounts/remove/(?P<pk>[\d+])$', views.update_account, name='add-account'),
-    #url(r'^accounts/pinverify/(?P<pk>[\d+])$', views.update_account, name='pinverify'),
-    
+    # url(r'^accounts/settings/(?P<pk>[\d+])$', views.account, name='account-settings'),
+    # url(r'^accounts/add/$', views.update_account, name='add-account'),
+    # url(r'^accounts/remove/(?P<pk>[\d+])$', views.update_account, name='add-account'),
+    # url(r'^accounts/pinverify/(?P<pk>[\d+])$', views.update_account, name='pinverify'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,48 +1,114 @@
+from smtplib import SMTPException
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.urls import reverse
 from django.urls.base import reverse_lazy
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 
 from app.forms import UserRegisterForm
-from app.models import MembershipType
-
+from app.models import MembershipType, Profile, Membership
+from app.tokens import account_activation_token
 
 User = get_user_model()
+
+
 class HomeView(TemplateView):
     template_name = 'app/home.html'
-    
+
     def get_context_data(self, **kwargs):
         ctx = super(HomeView, self).get_context_data(**kwargs)
         for x in MembershipType.objects.all():
             ctx[x.name] = x
-        #print('ctx:', ctx)
+        # print('ctx:', ctx)
         return ctx
-    
+
 
 class RegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('register_done')
-    
+
     def form_valid(self, form):
-        #return CreateView.form_valid(self, form)
+        # return CreateView.form_valid(self, form)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = form.cleaned_data.get('email')
             user.is_active = False
             user.save()
+            # create profile
+            Profile(user=user).save()
+
             # send validate email
-        
+
+            # collect site name
+            site_name = get_current_site(self.request)
+            # todo: change hard code subject
+            subject = 'Activate account'
+
+            # generate message
+            print(urlsafe_base64_encode(force_bytes(user.pk)))
+            message = render_to_string('app/account_activation_email.html', {
+                'user': user,
+                'domain': site_name.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode("utf-8"),
+                'token': account_activation_token.make_token(user),
+            })
+
+            # here username is email
+            email_address = user.username
+
+            # create email
+            email = EmailMessage(
+                subject, message, to=[email_address]
+            )
+
+            # send email
+            try:
+                email.send()
+            except SMTPException as e:
+                # todo: need to handle exception if email not send
+                pass
+
         return super(RegisterView, self).form_valid(form)
 
-        
-        
-    
+
 class SubsriptionView(TemplateView):
     pass
 
-    
+
 class ProfileView(TemplateView):
     pass
 
+
+class ActivateAccount(View):
+    template_name = "registration/invalid_activation.html"
+
+    def get(self, request, uidb64, token):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64.encode("utf-8")))
+            user = User.objects.get(pk=uid)
+        except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+        if user is not None and account_activation_token.check_token(user, token):
+            user.is_active = True
+            user.save()
+            login(request, user)
+            profile = user.profile
+            if profile.day_to_live <= 0:
+                membership_type, created = MembershipType.objects.get_or_create(name='Free')
+                membership = Membership(user=user, membership_type=membership_type)
+                membership.save()
+                membership.membership_type.add(membership_type)
+                profile.day_to_live = membership_type.day_to_live
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, self.template_name)
