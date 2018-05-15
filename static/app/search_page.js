b579f68b2b22fb72d04c7920db6be2b4fe38d440
Sharif Chowdhury <sharif.cse.hstu@gmail.com>
2018-05-15 17:49:23 +0600

    update search form ui

--- static/app/search_page.js
+++ static/app/search_page.js
@@ -65,12 +65,72 @@ $(document).ready(function () {
 
 
     $("#new_search").click(function () {
+
+        $('#advance_search_data').hide();
+        $('#search_keyword').value = '';
+        $('#search_location').value = '';
+        $('#search_industry').value = '';
+        $('#search_company').value = '';
+        $('#search_title').value = '';
+        $('#search_name').value = '';
+        $('#search_url').value = '';
+        $('#search_sales').value = '';
+
         $('#add_search').modal('show');
     });
 
 
+    $("#advanced_search_toggle").click(function () {
+        $('#advanced_search_toggle').hide();
+        $('#advance_search_data').show();
+    });
 
 
+    $("#add_search_task").click(function (event) {
+        event.preventDefault();
+
+        error = 0
+
+        if ($.trim($('#search_name')[0].value) == '') {
+            $('#search_name').parent().addClass('has-error');
+            error = 1;
+        }else{
+            $('#search_name').parent().removeClass('has-error');
+        }
+
+
+        if ($('#search_keyword').is(":visible")) {
+            if ($.trim($('#search_keyword')[0].value) == '') {
+                $('#search_keyword').parent().addClass('has-error');
+                error = 2;
+            }
+        }
+        if (error!=2){
+            $('#search_keyword').parent().removeClass('has-error');
+        }
+        if ($('#search_url').is(":visible")) {
+            if ($.trim($('#search_url')[0].value) == '') {
+                $('#search_url').parent().addClass('has-error');
+                error = 3;
+            }
+        }
+        if (error!=3){
+            $('#search_url').parent().removeClass('has-error');
+        }
+
+        if ($('#search_sales').is(":visible")) {
+            if ($.trim($('#search_sales')[0].value) == '') {
+                $('#search_sales').parent().addClass('has-error');
+                error = 4;
+            }
+        }
+        if (error!=4){
+             $('#search_sales').parent().removeClass('has-error');
+        }
+        if (error==0)
+            $('#add_search').submit();
+
+    });
 
 
 });
