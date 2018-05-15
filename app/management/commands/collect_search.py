7c4795865d34c22b9fb4a2162805fa24b2aed078
Sharif Chowdhury <sharif.cse.hstu@gmail.com>
2018-05-15 12:55:21 +0600

    add collect_search command

---
+++ app/management/commands/collect_search.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'collect_search : it will process all pending search results'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        print('Custom search cmd')
+        pass
