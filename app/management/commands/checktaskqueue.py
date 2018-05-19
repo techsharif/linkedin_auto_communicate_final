6b9ad33239726320ee4083bf0ef7ba1d64810752
techsharif <sharif.cse.hstu@gmail.com>
2018-05-19 22:50:23 +0600

    update the checktaskqueue

--- app/management/commands/checktaskqueue.py
+++ app/management/commands/checktaskqueue.py
@@ -13,7 +13,23 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
+        pending_task_list = BotTask.objects.exclude(status__in=[BotTaskStatus.DONE, BotTaskStatus.ERROR])
+        for pending_task in pending_task_list:
+            if pending_task.owner.status:
+                queue_type = ContentType.objects.get_for_model(pending_task)
+                task_queue = TaskQueue.objects.filter(object_id=pending_task.id, queue_type=queue_type)
+                if task_queue:
+                    task = task_queue[0]
+                    if task.due_date <= timezone.now():
+                        pending_task.status = task.status
+                        pending_task.save()
+                else:
+                    TaskQueue(content_object=pending_task).save()
+
+        self.check_taskqueue()
+
+    def check_taskqueue(self):
         task_queues = TaskQueue.objects.filter(status=BotTaskStatus.QUEUED).all()
         for task in task_queues:
             BotTask.objects.create(name=task.content_object, task_type=task.queue_type,
-                                   owner=task.owner, status=task.status, extra_info={'id':task.queue_type_id})
+                                   owner=task.owner, status=task.status, extra_info={'id': task.queue_type_id})
