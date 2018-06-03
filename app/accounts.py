60ef9ae675ec18951e072a521cd5c8156a808ea3
Shivam Sehgala <sehgal@Shivams-MacBook-Pro.local>
2018-06-03 21:29:44 +0530

    change page dir

--- app/accounts.py
+++ app/accounts.py
@@ -365,7 +365,7 @@ class AccountCampaign(AccounMessenger):
 
 @method_decorator(decorators, name='dispatch')
 class AccountSearch(View):
-    template_name = 'account/account_search.html'
+    template_name = 'v2/account/account_search.html'
 
     def get(self, request, pk):
         searches = Search.objects.filter(owner__pk=pk)
