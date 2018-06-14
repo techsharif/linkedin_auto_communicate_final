00def04cb5251bb638dfa7cba644dd3ac5ab3a3c
chalove <loveun1988@outlook.com>
2018-06-14 15:55:39 +0800

    #196 old logic

--- app/management/commands/update_linkedinuser.py
+++ app/management/commands/update_linkedinuser.py
@@ -1,22 +1,68 @@
 from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from app.models import MembershipType, Membership, LinkedInUser
+from django.utils import timezone
+import datetime
+from django.conf import settings
+
+User = get_user_model()
 
 
 class Command(BaseCommand):
-    
-    
+
     def add_arguments(self, parser):
         pass
-        
-    
+
     def handle(self, *args, **options):
-        
-        # check if nay Profile has days_to_live is = 0 , set its User's status to 'inactive'
-        #    else deduct it 1
-         
-        # check if any profile - days_to_live due in next x days, send and email to ask them to make a new subscription
-    
-        # make a summary email to admin:
-        #     those are expired 
-        #     Those are due
+        print('first')
         pass
-    
+
+    def check_expired_user(self):
+        users = User.objects.all()
+        memberships = Membership.objects.all()
+
+        expired_users = []
+        expiring_users = []
+        for membership in memberships:
+            try:
+                user = membership.user
+                join_date = user.date_joined
+                live_date = membership.membership_type.day_to_live
+                now = timezone.now()
+                expiring_date = join_date + datetime.timedelta(days=live_date-3)
+                expired_date = join_date + datetime.timedelta(days=live_date)
+                if now > expired_date:
+                    self.send_three_day_email(user)
+                elif now > expiring_date:
+                    self.send_expired_email(user)
+                else:
+                    pass
+            except Exception as e:
+                print('------>', e)
+
+    def send_three_day_email(self, user):
+        subject = 'Getting started with {0}'.format(settings.SITE_TITLE)
+        message = render_to_string('app/account_activate_expiring_email.html', {
+            'current_date': datetime.datetime.now().strftime('%Y-%m-%d'),
+            'site_title': settings.SITE_TITLE,
+            'user': user,
+        })
+        user.email_user(subject, message)
+        pass
+
+    def send_expired_email(self, user):
+        subject = 'Getting started with {0}'.format(settings.SITE_TITLE)
+        message = render_to_string('app/account_activate_expired_email.html', {
+            'current_date': datetime.datetime.now().strftime('%Y-%m-%d'),
+            'site_title': settings.SITE_TITLE,
+            'user': user,
+        })
+        user.email_user(subject, message)
+        pass
+
+    def delete_linkedin_user(self, user):
+        linkedin_users = LinkedInUser(user=user)
+        for linkedin_user in linkedin_users:
+            linkedin_user.is_deleted = True
+            linkedin_user.save()
+        pass
